c88a7a772a7be69068927b521d5e57f1b9181b16
Fejiroofficial <houseofjiro@gmail.com>
2018-10-03 12:59:49 +0100

    ft- consuming get all menu This is a commit for getting all food items on the menu.
    [Delivers #160945059]

---
+++ UI/js/getMenu.js
@@ -0,0 +1,44 @@
+window.addEventListener('load', (event) => {
+  event.preventDefault();
+  const meal = document.querySelector('div.meal-box-content');
+
+  const load = (foodMenu) => {
+    foodMenu.forEach((element) => {
+      const itemName = element.item_name;
+      const price = element.price;
+      const foodUrl = element.food_url;
+      const foodOnMenu = `
+      <div class="meal-single">
+      <div class="meal-img" style="background-image: url(${foodUrl})"></div>
+      <div class="meal-info">
+        <div class="meal-info-title">${itemName}</div>
+        <div class="meal-info-price">
+          <span>₦</span>${price}</div>
+        <div class="meal-info-cart">
+          <div class="to-cart-btn">Add to cart</div>
+        </div>
+      </div>
+    </div>`;
+
+      meal.innerHTML += foodOnMenu;
+    });
+  };
+
+  fetch('https://food-fast-app.herokuapp.com/api/v1/menu', {
+    method: 'get',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    },
+  })
+    .then((response) => {
+      response.json()
+        .then((responseObject) => {
+          const foodMenu = responseObject.menu;
+          load(foodMenu);
+        })
+        .catch((err) => {
+          throw Error(err);
+        });
+    });
+});
